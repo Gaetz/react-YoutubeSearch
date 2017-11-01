9a0491f82dcb9f4ffc1fa1b73f1a6b4400b9a5bf
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-11-01 12:01:40 +0100

    Initial search

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar'
-
 const API_KEY = 'AIzaSyAWYARh9wb78qKqbA5mp_JPdOXVq8myU9s';
 
-// Create a new component. Should produce some html.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term:'zelda'}, (videos) => {
+            this.setState({videos});
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
-// Take this component's generated html and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
