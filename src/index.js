342d504ef91032768fdc6b397625fff190e85bad
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-11-01 09:52:25 +0100

    Create component files and integrate youtube search api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAWYARh9wb78qKqbA5mp_JPdOXVq8myU9s';
+
 // Create a new component. Should produce some html.
 const App = () => {
     return <div>Oyo!</div>
